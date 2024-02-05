ff3fc5c7ce7911433441a1068b35a244f23daa23
Omkar Sunil Kulkarni <139356827+omkarkulkarnii@users.noreply.github.com>
2024-02-05 16:03:14 +0530

    Add files via upload

---
+++ Codes/src/components/Embed.jsx
@@ -0,0 +1,11 @@
+function Embed(){
+return(
+    <>
+    <h8>Understand the procedure</h8>
+    <iframe width="350" height="200" src="https://www.youtube.com/embed/AFDYnd-XPa8?list=PLu71SKxNbfoDqgPchmvIsL4hTnJIrtige" title="Custom hooks in react | currency Project" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    
+    
+    </>
+)
+}
+export default Embed;
---
+++ Codes/src/components/index.js
@@ -0,0 +1,4 @@
+//bring all components here 
+import InputBox from "./InputBox";
+import Embed from "./Embed";
+export {InputBox, Embed}
